ff9b6493b7050941b779c7d94512b6aaff81648b
Rickey <rickseleven@gmail.com>
2016-10-31 16:30:24 -0700

    add new_task

--- views.py
+++ views.py
@@ -3,7 +3,7 @@
 import sqlite3
 from functools import wraps
 
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session, url_for, g
 
 # config
 
@@ -44,3 +44,34 @@ def login():
             flash('Welcome!')
             return redirect(url_for('tasks'))
     return render_template('login.html')
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+    g.db = connect_db()
+    cur = g.db.execute('select name, due_date, priority, task_id from tasks where status=1')
+    open_tasks = [dict(name=row[0], dur_date=row[1], priority=row[2], task_id=row[3]) for row in cur.fetchall()]
+    cur = g.db.execute('select name, dur_date, priority, task_id from tasks where status=0')
+    closed_tasks = [dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('tasks.html', form=AddTaskForm(request.form), open_tasks=open_tasks, closed_tasks=closed_tasks)
+
+# Add new tasks
+@app.route('/add/', methods=['POST'])
+@login_required
+def new_task():
+    g.db = connect_db()
+    name = request.form['name']
+    date = request.form['due_date']
+    priority = request.form['priority']
+    if not name or not date or not priority:
+        flash("All fields are required.  Please try again.")
+        return redirect(url_for('tasks'))
+    else:
+        g.db.execute('insert into tasks (name, due_date, priority, status) values (?, ?, ?, 1)', [request.form['name'], request.form['due_date'], request.form['priority']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted.  Thanks.')
+        return redirect(url_for('tasks'))
+
+# Mark tasks as complete
