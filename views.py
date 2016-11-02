467e9aa2ef8ee0138bb6357135f924c1696f29ec
Rickey <rickseleven@gmail.com>
2016-11-02 14:32:33 -0700

    Finished tasks.html

--- views.py
+++ views.py
@@ -75,3 +75,23 @@ def new_task():
         return redirect(url_for('tasks'))
 
 # Mark tasks as complete
+@app.route('/complete/<int:task_id>/')
+@login_required
+def complete(task_id):
+    g.db = connect_db()
+    g.db.execute('update tasks set status = 0 where task_id='+str(task_id))
+    g.db.commit()
+    g.db.close()
+    flash('The task was marked as complete.')
+    return redirect(url_for('tasks'))
+
+# Delete tasks
+@app.route('/delete/<int:task_id>/')
+@login_required
+def delete_entry(task_id):
+    g.db = connect_db()
+    g.db.execute('delete from tasks where task_id='+str(task_id))
+    g.db.commit()
+    g.db.close()
+    flash('The task was deleted.')
+    return redirect(url_for('tasks'))
