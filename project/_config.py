01a100e5004d95d0ccd4eb779963e199422c541b
Rickey <rickseleven@gmail.com>
2016-11-24 11:52:05 -0800

    deploy to heroku

--- project/_config.py
+++ project/_config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 CSRF_ENABLED = True
 SECRET_KEY = 'rM\xb1\xdc\x12o\xd6i\xff+9$T\x8e\xec\x00\x13\x82.*\x16TG\xbd'
-DEBUG = True
+DEBUG = False
 
 # define the full path for hte database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 # run.py
 
+import os
 from project import app
-app.run(debug=False)
-#app.run(debug=True)
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
 
