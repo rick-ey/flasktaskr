139c9c256287d82dadc765d69283272d9df3ba62
Rickey <rickseleven@gmail.com>
2016-11-26 20:48:52 -0800

    added tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ import unittest
 
 from project import app, db
 from project._config import basedir
-from project.models import User
+# from project.models import User
 
 TEST_DB = 'test.db'
 
@@ -59,6 +59,11 @@ class MainTests(unittest.TestCase):
         self.assertEquals(response.status_code, 500)
         self.assertNotIn(b'ValueError: Invalid salt', response.data)
         self.assertIn(b'Something went terribly wrong.', response.data)'''
+
+    def test_index(self):
+        """ Ensure flask was set up correctly. """
+        response = self.app.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
